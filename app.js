d6b69c42f833251c8227fcd8526f0f815bc08ea5
boneseggsbones <mccurleyj@gmail.com>
2014-05-31 14:00:28 -0700

    created view constructr fnc     *added render car proto

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ window.addEventListener("load", initialize)
 function initialize(){
     console.log("meow")
 }
+
+
+function View(){}
+
+View.prototype = {
+    renderCar = function(){
+        console.log("render car")
+    }
+}
