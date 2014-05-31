cff84ff65d92afdd0b10f11911c69a9327a3c5d6
boneseggsbones <mccurleyj@gmail.com>
2014-05-31 14:16:52 -0700

    added controller and car constructors     *car is initialized with key, pos, and win states     *car prototype has updatePos function     *scaffold controller functionality

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-window.addEventListener("load", initialize)
+window.addEventListener( "load", initialize )
 
 function initialize(){
-    console.log("meow")
+    console.log( "meow" )
 }
+
+
+
+function Car( key ){
+    this.key = key
+    this.position = 0
+    this.win = false
+}
+
+Car.prototype = {
+    updatePosition: function(){
+        this.position += 1
+    }
+}
+
+
+
+function Controller(){}
+
+
+Controller.prototype = {
+    bindEventListeners: function(){},
+    updateCarPosition: function(){},
+    checkForWinner: function(){},
+    sendRenderInfo: function(){} 
+}
+
 
 
 function View(){}
 
 View.prototype = {
-    renderCar = function(){
-        console.log("render car")
+    renderCar: function(){
+        console.log( "render car" )
     }
 }
+
+
